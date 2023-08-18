1ad737b34f7aaec8e29a08dc668053fdb8b2d636
SARITA-GOCHHAYAT <saritagochhayat7377@gmail.com>
2023-08-18 23:09:44 +0530

    time pass

---
+++ index.js
@@ -0,0 +1,152 @@
+
+let random=document.getElementById("random");
+let input=document.getElementById("num");
+let message=document.getElementById("messagep");
+let a=Math.random()*100;
+random.innerHTML=Math.floor(a);
+let bestsc=document.getElementById("bestscorenum");
+let sc=document.getElementById("currentscorenum");
+let b=Number(random.innerHTML);
+let count=0;
+let link=document.querySelector("a");
+let button=document.getElementById("ting");
+// let highscore=localStorage.getItem("top");
+// if(highscore == null){
+//   let temp=0;
+//   localStorage.setItem("top",JSON.stringify(temp));
+// }else{
+//   temp=JSON.parse(localStorage.getItem(top));
+//   bestsc.innerHTML=sc.innerHTML;
+// }
+button.addEventListener("click",function(){
+    let c=input.value;
+    if(c>0 && c<100){
+ if(b==c){
+    random.style.color="green";
+    message.style.color="aqua";
+    message.innerHTML="You Won";
+button.style.display="none";
+    let W = window.innerWidth;
+let H = window.innerHeight;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const maxConfettis = 150;
+const particles = [];
+
+const possibleColors = [
+  "DodgerBlue",
+  "OliveDrab",
+  "Gold",
+  "Pink",
+  "SlateBlue",
+  "LightBlue",
+  "Gold",
+  "Violet",
+  "PaleGreen",
+  "SteelBlue",
+  "SandyBrown",
+  "Chocolate",
+  "Crimson"
+];
+
+function randomFromTo(from, to) {
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+function confettiParticle() {
+  this.x = Math.random() * W; // x
+  this.y = Math.random() * H - H; // y
+  this.r = randomFromTo(11, 33); // radius
+  this.d = Math.random() * maxConfettis + 11;
+  this.color =
+    possibleColors[Math.floor(Math.random() * possibleColors.length)];
+  this.tilt = Math.floor(Math.random() * 33) - 11;
+  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+  this.tiltAngle = 0;
+
+  this.draw = function() {
+    context.beginPath();
+    context.lineWidth = this.r / 2;
+    context.strokeStyle = this.color;
+    context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+    context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+    return context.stroke();
+  };
+}
+
+function Draw() {
+  const results = [];
+
+  // Magical recursive functional love
+  requestAnimationFrame(Draw);
+
+  context.clearRect(0, 0, W, window.innerHeight);
+
+  for (var i = 0; i < maxConfettis; i++) {
+    results.push(particles[i].draw());
+  }
+
+  let particle = {};
+  let remainingFlakes = 0;
+  for (var i = 0; i < maxConfettis; i++) {
+    particle = particles[i];
+
+    particle.tiltAngle += particle.tiltAngleIncremental;
+    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+    particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+    if (particle.y <= H) remainingFlakes++;
+
+    // If a confetti has fluttered out of view,
+    // bring it back to above the viewport and let if re-fall.
+    if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+      particle.x = Math.random() * W;
+      particle.y = -30;
+      particle.tilt = Math.floor(Math.random() * 10) - 20;
+    }
+  }
+
+  return results;
+}
+
+window.addEventListener(
+  "resize",
+  function() {
+    W = window.innerWidth;
+    H = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  },
+  false
+);
+
+// Push new confetti objects to `particles[]`
+for (var i = 0; i < maxConfettis; i++) {
+  particles.push(new confettiParticle());
+}
+
+// Initialize
+canvas.width = W;
+canvas.height = H;
+Draw();
+ }else if(b<c){
+    message.innerHTML="Suspected number is lesser,Try again.";
+ input.value=null; 
+}else{
+    message.innerHTML="Suspected number is greater,Try again";
+ input.value=null; 
+}
+}else{
+    alert("put the number between 0 to 100");
+    input.value=null;
+}
+count++;
+sc.innerHTML=count;
+});
+
+
+
+
+
+
+
